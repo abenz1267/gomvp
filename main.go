5b06c041c898822fd72cd245188e51d112adae74
Andrej Benz <andrej.benz@bader-jene.de>
2022-10-25 11:53:59 +0200

    feat: added git status checks before performing any further actions

--- main.go
+++ main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"log"
 	"os"
+	"os/exec"
 
 	"github.com/abenz1267/gomvp/internal"
 )
 
 func main() {
+	checkGitConditions()
+
 	args := os.Args
 	internal.Move(args[1], args[2])
+}
+
+func checkGitConditions() {
+	cmd := exec.Command("git", "rev-parse", "--is-inside-work-tree")
+
+	err := cmd.Run()
+	if err != nil {
+		log.Println(err)
+		log.Fatal("Are you running gomvp inside a git directory?")
+	}
+
+	cmd = exec.Command("git", "status", "--porcelain")
+
+	out, err := cmd.Output()
+	if err != nil {
+		panic(err)
+	}
+
+	if string(out) != "" {
+		log.Fatal("Make sure there are no uncommited changes before running gomvp!")
+	}
 }
